d93fec328723169a3bc327fa5be43675dc307a5e
Brian J Brennan <brianloveswords@gmail.com>
2012-11-06 22:17:48 -0500

    Basic pipeline thing working

--- fpipe.js
+++ fpipe.js
@@ -1,10 +1,58 @@
 const Stream = require('stream');
-module.exports = function fpipe(fn) {
-  var stream = new Stream;
+const streamProto = Stream.prototype;
+
+const pipelineProto = Object.create(streamProto);
+pipelineProto.fpipe = function fpipe(endpoint, type) {
+  if (!endpoint.pipe && typeof endpoint === 'function')
+    endpoint = pipeline(endpoint);
+  return streamProto.pipe.call(this, endpoint);
+};
+
+pipelineProto.begin = function begin() {
+  this.fn(function (err) {
+    var args = [].slice.call(arguments, 1);
+    if (err)
+      return this.emit('error', err);
+    this.emit.bind(this, 'data').apply(null, args);
+    this.emit('end');
+  }.bind(this));
+};
+
+pipelineProto.write = function write() {
+  var value = this.fn.apply(this, arguments);
+  if (typeof value !== 'undefined')
+    this.emit('data', value);
+};
+
+pipelineProto.end = function end() {
+  if (arguments.length)
+    return this.write.apply(this, arguments);
+};
+
+pipelineProto.send = function send() {
+  this.emit.bind(this, 'data').apply(null, arguments);
+};
+
+const pipeline = function pipeline(fn) {
+  var stream;
+  if (fn.pipe) // assume it's a stream
+    fn.fpipe = stream.fpipe;
+  stream = Object.create(pipelineProto);
+  stream.fn = fn;
+  stream.readable = true;
   stream.writable = true;
-  stream.write = function write(v) {
-    this.emit('data', fn(v));
-  }
-  stream.end = stream.write;
   return stream;
 };
+
+pipeline.disperse = function (x) {
+  x.forEach(this.send.bind(this));
+};
+
+pipeline.log = function (description) {
+  return function (x) {
+    console.log('\n---', description, '::', x, '---');
+    return x;
+  }
+};
+pipeline.logger = console.log;
+module.exports = pipeline;
--- test/composition.test.js
+++ test/composition.test.js
@@ -1,8 +1,9 @@
 var test = require('tap').test;
 var iterators = require('./iterators');
 var istream = require('..');
+var pipeline = require('../fpipe');
 var StreamString = require('./streamstring');
-
+var Stream = require('stream');
 var fib = iterators.fib;
 var letter = iterators.letter;
 var natural = iterators.natural;
@@ -12,52 +13,51 @@ var random = iterators.random;
 function inc(x) {
   return ++x;
 }
+function mod(x) {
+  return x % 2;
+}
+function take(num) {
+  return function () {
+    return (--num >= 0);
+  }
+}
+function even(x) {
+  return x % 2 == 0;
+}
+function elem(x) {
+  return function (obj) { return obj[x] }
+}
 
-test('fpipe', function (t) {
+function format(fmt) {
+  return function(x) {
+    return require('util').format(fmt, x);
+  }
+}
+function filter() {  }
 
+test('pipeline basic', function (t) {
+  var DATABASE = [
+    {first: 'Steve', last: 'Doucheman', age: 44},
+    {first: 'Bill', last: 'Paxton', age: 12},
+    {first: 'Rich', last: 'Martin', age: 19},
+    {first: 'Timm', last: 'Glisdao', age: 18},
+    {first: 'Ilde', last: 'Yolsla', age: 13},
+    {first: 'Usne', last: 'Lados', age: 32},
+  ];
+
+  function databaseCall(callback) {
+    process.nextTick(function () {
+      callback(null, DATABASE);
+    });
+  }
+
+  var alaska = pipeline(databaseCall);
+  alaska
+    .fpipe(pipeline.disperse)
+    .fpipe(elem('first'))
+    .fpipe(format('%s!\n'))
+    .fpipe(process.stdout);
+  alaska.begin()
+
+  t.end();
 });
-
-// test('pipelining', function (t) {
-//   var iter = natural;
-
-//   var util = require('util');
-
-//   function inc(x) {
-//     return ++x;
-//   }
-
-//   function even(x) {
-//     return x % 2 == 0;
-//   }
-
-//   function format(fmt) {
-//     return function (x) {
-//       return x + '!';
-//     }
-//   }
-
-//   function takeWhile(fn) {
-//     function (x) {
-//       if (!fn(x))
-//         return null;
-//       return true
-//     }
-//   }
-
-//   function lt(num) {
-//     return function (x) {
-//       return x < num;
-//     }
-//   }
-
-//   iterstream(natural())
-//     .fpipe(transform(inc))
-//     .fpipe(filter(even))
-//     .fpipe(format('%s!'))
-//     .fpipe(takeWhile( lt(5) ))
-//     .pipe(bufferstream(8192))
-//     .pipe(process.stdout)
-
-//   t.end();
-// });
-
